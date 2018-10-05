517940ac60cd852c852acb49ead185a38531a8cb
Ugputu <eugene@iteye.ru>
2018-10-05 21:43:25 +0300

    Add log function

--- src/catcher.php
+++ src/catcher.php
@@ -4,8 +4,10 @@ namespace dsda\catcher;
 
 class catcher {
 
-	function __construct($config){
-		error_reporting(0);
+	private $config = null;
+	
+	function __construct($config=null){
+		error_reporting(-1);
 		set_error_handler(array($this,'errorHandler'));
 		register_shutdown_function(array($this,'fatalErrorShutdownHandler'));
 		$this->config = $config;
@@ -13,7 +15,7 @@ class catcher {
 
 	function errorHandler($code, $message, $file, $line) {
 		$this->debug(array('Error'=>$code,'Message'=>$message,'In file'=>$file,'On line'=>$line));
-		exit();
+		// exit(); // no need exit it's can be halted on @$_GET['qwe']...
 	}
 
 	function fatalErrorShutdownHandler() {
@@ -23,9 +25,8 @@ class catcher {
 		}
 	}
 
-	// Example: throw new Exception('Division by zero');
-	function debug($smth,$vardump=false,$backtrace=true){
-		echo "<pre>";
+	function prepare_output($smth,$vardump=false,$backtrace=false){
+		ob_start();
 		echo "================== DEBUG MESSAGE ==================\n";
 		
 		if (!$vardump) {
@@ -39,7 +40,26 @@ class catcher {
 			echo "================== BACKTRACE ==================\n";
 			print_r(debug_backtrace());
 		}
-		echo "</pre>";
+		$log = ob_get_contents();
+		ob_end_clean();
+		return $log;
+	}
+	
+	function log($smth,$vardump=false,$backtrace=false){
+		if (($this->config!==null && $this->config->get('debug')==true) || $backtrace==true) {
+			$backtrace=true;
+		}
+		$log = $this->prepare_output($smth,$vardump,$backtrace);
+		echo '<pre>'.$log.'</pre>';
+	}
+	
+	// Example: throw new Exception('Division by zero');
+	function debug($smth,$vardump=false,$backtrace=false){
+		if (($this->config!==null && $this->config->get('debug')==true) || $backtrace==true) {
+			$backtrace=true;
+		}
+		$log = $this->prepare_output($smth,$vardump,$backtrace);
+		@file_put_contents($this->config->get('path').'/logs/catcher.txt', $log, FILE_APPEND);
 	}
 
 }
